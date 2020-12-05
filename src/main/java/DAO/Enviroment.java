c85f422526d80fd09482be022848498422fa6201
Francisndungu2 <francianmbugua@gmail.com>
2020-12-05 22:49:13 +0300

    addcreatedrop.sql

--- src/main/java/DAO/Enviroment.java
+++ src/main/java/DAO/Enviroment.java
@@ -1,6 +1,6 @@
 package DAO;
 
-public class Environment {
-    public static final String USER_NAME ="francisndungu2";
+class Environment {
+    public static final String USER_NAME ="franchising2";
     public static final String PASS_WORD ="franc@34";
 }
