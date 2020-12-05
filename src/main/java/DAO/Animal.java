f72962dbe2e09c02b339e81a9cb00b2a5b276aa9
Francisndungu2 <francianmbugua@gmail.com>
2020-12-05 14:24:16 +0300

    addgetid

--- src/main/java/DAO/Animal.java
+++ src/main/java/DAO/Animal.java
@@ -1,4 +1,27 @@
 package DAO;
 
+import org.sql2o.Connection;
+import org.sql2o.Sql2oException;
+
+import java.util.List;
+import java.util.Objects;
+
 public class Animal {
+    public int id;
+    public String name;
+    public String type;
+
+    //<....................this is my constant................................................................................>
+    public static final String ANIMAL_TYPE = "Non-endangered";
+    public Animal(String name) {
+
+
+        this.name = name;
+        this.type = ANIMAL_TYPE;
+    }
+
+    public int getId() {
+
+        return id;
+    }
 }
