6bc70d01d705d8c2f45e3b393739274f370d7c5a
Francisndungu2 <francianmbugua@gmail.com>
2020-12-05 14:54:40 +0300

    addfindanimalbyid

--- src/main/java/DAO/Animal.java
+++ src/main/java/DAO/Animal.java
@@ -33,4 +33,48 @@ public class Animal {
 
         return this.type;
     }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Animal animal = (Animal) o;
+        return id == animal.id &&
+                Objects.equals(name, animal.name) &&
+                Objects.equals(type, animal.type);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, type);
+    }
+
+    public void save () {
+        if( this.name == null || this.name.trim().isEmpty()){
+            throw new NullPointerException("name cannot be null");
+        }
+        String sql = "INSERT INTO animals (type, name) VALUES (:type, :name);";
+        try(Connection con = DB.sql2o.open()){
+            this.id = (int) con.createQuery(sql, true)
+                    .addParameter("type", this.type)
+                    .addParameter("name", this.name)
+                    .executeUpdate()
+                    .getKey();
+        }
+        public static List<Animal> all() {
+            String sql = "SELECT * FROM animals WHERE type = :type;";
+            try(Connection con = DB.sql2o.open()) {
+                return con.createQuery(sql)
+                        .addParameter("type",ANIMAL_TYPE)
+                        .throwOnMappingFailure(false)
+                        .executeAndFetch(Animal.class);
+            }
+            public static Animal findById (int id) {
+                String sql = "SELECT * FROM animals WHERE id = :id AND type = :type;";
+                try (Connection con = DB.sql2o.open()){
+                    return con.createQuery(sql)
+                            .addParameter("id", id)
+                            .addParameter("type", ANIMAL_TYPE)
+                            .throwOnMappingFailure(false)
+                            .executeAndFetchFirst(Animal.class);
+                }
+    }
 }
